50214ec68e6d90d5784d882c0d26e00793a5ab96
hendzk <hendrikravansyah0@gmail.com>
2024-09-13 20:23:38 +0700

    upload via termux

---
+++ QIO VIP/settings.js
@@ -0,0 +1,17 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['628xxx']
+global.botname = 'Alwaysaqioo'
+global.namaowner = 'Alwaysaqioo'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Alwaysaqioo"
+global.sticker2 = "🌜"
+global.apikey = '_'
+global.capikey = '_'
+global.domain = '_'
+global.eggsnya = '_'
+global.location = '_'
+global.adana = '628XX'
+global.aqris = 'https://telegra.ph/file/c1dfbad852cbfa1341c01.jpg'
+global.aovo = '628XX'
+global.agopay = '628XX'
